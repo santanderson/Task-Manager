05c987294016b0a28df67c257451b72b03845585
Anderson Ryan <santoryan1408@gmail.com>
2022-03-06 10:34:49 -0300

    write function added

--- main.js
+++ main.js
@@ -2,16 +2,31 @@ function Add(){
     this.name = document.querySelector('.title');
     this.content = document.querySelector('.content');
 
-    this.createE = () =>{
+    this.createE = () => {
         const checkBtn = document.createElement('input');
         const title = document.createElement('h2');
         const content = document.createElement('p');
         const removeBtn = document.createElement('button');
+        const li = document.createElement('li');
+        const ulTasks = document.querySelector('.tasks');
         checkBtn.type = 'checkbox';
         removeBtn.type = 'button';
+
+        this.write(checkBtn, title, content, removeBtn, li, ulTasks);
     };
+
+    this.write = (checkBtn, title, content, removeBtn, li, ulTasks) => {
+        title.innerHTML = this.name.value;
+        content.innerHTML = this.content.value;
+
+        li.appendChild(checkBtn);
+        li.appendChild(title);
+        li.appendChild(content);
+        li.appendChild(removeBtn);
+        ulTasks.appendChild(li);
+    }
 }
 
 const func = new Add();
 const addBtn = document.querySelector('.add');
-addBtn.addEventListener('click', func.createE());
+addBtn.addEventListener('click', func.createE);
