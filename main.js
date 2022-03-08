af1bc6c7bf8032d2a999f7ab213b6410f6a22720
Anderson Ryan <santoryan1408@gmail.com>
2022-03-08 12:27:54 -0300

    basic style added

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ function Add(){
     this.createE = () => {
         const checkBtn = document.createElement('input');
         const title = document.createElement('h2');
+        const header = document.createElement('header');
         const content = document.createElement('p');
         const removeBtn = document.createElement('button');
         const li = document.createElement('li');
@@ -12,17 +13,18 @@ function Add(){
         checkBtn.type = 'checkbox';
         removeBtn.type = 'button';
 
-        this.write(checkBtn, title, content, removeBtn, li, ulTasks);
+        this.write(checkBtn, title, header, content, removeBtn, li, ulTasks);
     };
 
-    this.write = (checkBtn, title, content, removeBtn, li, ulTasks) => {
+    this.write = (checkBtn, title, header, content, removeBtn, li, ulTasks) => {
         title.innerHTML = this.name.value;
         content.innerHTML = this.content.value;
         removeBtn.innerHTML = 'Remove';
         removeBtn.addEventListener('click', this.delete);
 
-        li.appendChild(checkBtn);
-        li.appendChild(title);
+        li.appendChild(header);
+        header.appendChild(checkBtn);
+        header.appendChild(title);
         li.appendChild(content);
         li.appendChild(removeBtn);
         ulTasks.appendChild(li);
